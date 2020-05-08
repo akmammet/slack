467be8979cefe8b080dc4c3c5fa108bacd6fc2fd
akmammet <63406754+akmammet@users.noreply.github.com>
2020-05-08 12:33:30 -0700

    Update LogCall.java

--- src/LogCall.java
+++ src/LogCall.java
@@ -1,5 +1,7 @@
 public class LogCall {
     public static void main(String[] args) {
         System.out.println("hello from space");
+        String student = new Student(){
+            
     }
 }
